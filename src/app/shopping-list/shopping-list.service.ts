f40ccd83794e237dce379af11aef0b92d647c30b
Federico Arribas <farribas@emmsa.net>
2017-09-08 10:21:18 -0300

    Integrated add function to ingredients service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -7,12 +7,15 @@ export class ShoppingListService {
     new Ingredient('Tomatoes', 10)
   ];
 
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
   getIngredients() {
     return this.ingredients.slice();
   }
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 
 }
